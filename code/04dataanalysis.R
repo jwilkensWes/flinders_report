dadb29cdeb7f359a2cccb7a6af2a07d7700a3d13
Johnny Wilkens <jwilkens@wesleyan.edu>
2024-05-17 13:54:30 +1000

    finalreport created

--- code/04dataanalysis.R
+++ code/04dataanalysis.R
@@ -6,7 +6,7 @@
 ## data points
 load("data/cleandata/profileclosuredepth.Rdata")
 sea_rise_100 <- 0.77 # meters, rcp 8.5 2100
-sea_rise_50 <- 0.40 # meters, rcp 8.5 2050
+sea_rise_50 <- 0.23 # meters, rcp 8.5 2050
 cshore_length <- 1375 # meters
 berm_height <- 3.6 # meters
 rf <- 70.9/2 # meters
@@ -14,16 +14,11 @@ rf <- 70.9/2 # meters
 retreat_100 <- sea_rise_100 * (cshore_length / (berm_height + hc))
 ## 72.051
 retreat_50 <- sea_rise_50 * (cshore_length / (berm_height + hc))
-## 36.927
+## 21.522
 retreatflux_100 <- rf + retreat_100
 ## 107.501
 retreatflux_50 <- rf + retreat_50
-## 72.879
-
-
-
-
-
+## 56.972
 
 
 # wave climate
@@ -33,10 +28,11 @@ load("data/cleandata/flwave.Rdata")
 load("data/cleandata/hs_avg.Rdata")
 load("data/cleandata/hs_stdv.Rdata")
 
-## exceedence calculation
+## exceedance calculation
 h0.5 <- 2 * hs_stdv * sqrt(2 * log(1 / 0.5 ))
 ### 1.670 meters
 h0.1 <- 2 * hs_stdv * sqrt(2 * log(1 / 0.1 ))
 ### 3.044 meters
 h0.01 <- 2 * hs_stdv * sqrt(2 * log(1 / 0.01 ))
 ### 4.304 meters
+  
